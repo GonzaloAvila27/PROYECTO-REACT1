a31631283455c7fc4a599e1f15c957f90a8d5e3c
GonzaAv <gonzaloavila2727@gmail.com>
2022-07-14 22:08:23 -0300

    2

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='container'><button>Home</button><button>Shop</button><button>About Us</button><button>Contact US</button></div>
+  )
+}
+
+export default Navbar
