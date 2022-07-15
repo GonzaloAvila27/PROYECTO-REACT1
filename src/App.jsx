0ef155c0df5a0009e6f0a57413b8ec7b05154f68
GonzaAv <gonzaloavila2727@gmail.com>
2022-07-15 08:36:24 -0300

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import Navbar from '../components/Navbar'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
