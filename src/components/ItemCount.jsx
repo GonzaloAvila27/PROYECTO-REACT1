9e5815acaf74b8a41ca3247c60f69f180fe8032e
GonzaAv <gonzaloavila2727@gmail.com>
2022-09-08 00:43:40 -0300

    diooooos

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -27,7 +27,7 @@ const {cart} = useCartContext();
 return (
     <>   
         <div className="counter">
-            <button disabled={count <= 1 } onClick={decrease} className="counter__remove"> <i class="bi bi-dash"></i></button>
+            <button disabled={count <= 1 } onClick={decrease} className="counter__remove"><i class="bi bi-dash"></i></button>
             <span className='counter__number'>{count}</span>
             <button disabled = {count === stock}  onClick={increase} className="counter__add "> <i class="bi bi-plus-lg"></i> </button>  
             <span  disabled = {count === 0 }  onClick={handleAdd}><i className="counter__cart bi bi-cart-plus"></i>  </span>
